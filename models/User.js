ad1490a0f65101a0d6cd4df097ab365227d362f8
jcg <jcg.cristel@gmail.com>
2022-09-13 20:51:36 -0400

    created user model

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+
+const validateEmail = function(email) {
+    var regex = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+    return regex.test(email);
+}
+
+const UserSchema = new Schema({
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: [validateEmail, 'Email invalid']
+        },
+        thoughts: [{ 
+            type: Schema.Types.ObjectId, 
+            ref: 'Thought'
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
+    })
