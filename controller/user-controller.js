e9f21883ffebf5e9bbffa169bd9ceaef40106f5b
jcg <jcg.cristel@gmail.com>
2022-09-13 23:30:57 -0400

    added get, get by id, create for users

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -2,27 +2,60 @@ const { User } = require('../models');
 
 const UserController = {
     // Get all users
-    getAllUsers() {
+    getAllUsers(req, res) {
+        User.find({})
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({_id: -1})
+            .then(dbUserData => res.json(dbUserData))
+            .catch(e => {
+                console.log(e);
+                res.status(400).json(e);
+            });
 
     },
 
     // Get one user by id
-    getUserById() {
-
+    getUserById({params}, res) {
+        User.find({_id: params.id})
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .select('-__v')
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!'});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(e => {
+                console.log(e);
+                res.status(400).json(e);
+            });
     },
 
     // Create user
-    createUser() {
+    createUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(e => res.status(400).json(e));
 
     },
 
     // Update user by id
-    updateUser() {
-
+    updateUser({ params, body }, res) {
+        
     },
 
     // Delete user by id
     deleteUser() {
-        
+
     }
+
+    // BONUS: REMOVE A USER'S ASSOCIATED THOUGHTS WHEN DELETED
 }
--- models/User.js
+++ models/User.js
@@ -6,29 +6,39 @@ const validateEmail = function(email) {
 }
 
 const UserSchema = new Schema({
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-            trim: true
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            validate: [validateEmail, 'Email invalid']
-        },
-        thoughts: [{ 
-            type: Schema.Types.ObjectId, 
-            ref: 'Thought'
-        }],
-        friends: [{
-            type: Schema.Types.ObjectId,
-            ref: 'User'
-        }]
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
     },
-    {
-        toJSON: {
-            virtuals: true
-        }
-    })
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: [validateEmail, 'Email invalid']
+    },
+    thoughts: [{ 
+        type: Schema.Types.ObjectId, 
+        ref: 'Thought'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+},
+{
+    toJSON: {
+        virtuals: true
+    }
+})
+
+
+// Total friend count
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length();
+})
+
+const User = model('User', UserSchema);
+
+module.exports = User;
