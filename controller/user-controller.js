c362a1b3003181e99a71f542e80707329f661051
jcg <jcg.cristel@gmail.com>
2022-09-13 23:19:34 -0400

    base for user controllers

---
+++ controller/user-controller.js
@@ -0,0 +1,28 @@
+const { User } = require('../models');
+
+const UserController = {
+    // Get all users
+    getAllUsers() {
+
+    },
+
+    // Get one user by id
+    getUserById() {
+
+    },
+
+    // Create user
+    createUser() {
+
+    },
+
+    // Update user by id
+    updateUser() {
+
+    },
+
+    // Delete user by id
+    deleteUser() {
+        
+    }
+}
