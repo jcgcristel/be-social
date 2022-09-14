8e3e8249d6d80b141a9d1cb3ac8f4d65f1e09164
jcg <jcg.cristel@gmail.com>
2022-09-13 21:12:24 -0400

    added thought model, includes replyschema

---
+++ models/Thought.js
@@ -0,0 +1,54 @@
+const { Schema, model, Types } = require('mongoose');
+
+const ReactionSchema = new Schema({
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            min: [1, 'Character must be between 1-280'],
+            max: [280, 'Character must be between 1-280']
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: { 
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getter: true
+        }
+    }
+)
+
+const ThoughtSchema = new Schema({
+        thoughtText: {
+            type: String,
+            required: true,
+            min: [1, 'Character must be between 1-280'],
+            max: [280, 'Character must be between 1-280']
+        },
+        createdAt: { 
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String, ref: 'User.username'
+        },
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getter: true
+        }
+    }
+)
