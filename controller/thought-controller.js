4e78e31924c9fe760eec0ad4bb7561c0de5e0338
jcg <jcg.cristel@gmail.com>
2022-09-14 23:46:59 -0400

    added thought controllers

--- controller/thought-controller.js
+++ controller/thought-controller.js
@@ -5,10 +5,59 @@ const thoughtController = {
     getAllThought(req, res) {
         Thought.find({})
         .then(dbThoughtData => res.json(dbThoughtData))
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
+        .catch(e => {
+            console.log(e);
+            res.status(400).json(e);
         });
+    },
+
+    // get one thought by id
+    getThoughtbyId({ params }, res) {
+        Thought.findOne({ _id: params.id })
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(e => {
+                console.log(e);
+                res.status(400).json(e);
+            });
+    },
+
+    // create thought
+    createThought({ body }, res) {
+        Thought.create(body)
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(e => {
+                console.log(e);
+                res.status(400).json(e)
+            })
+    },
+
+    // update thought
+    updateThought({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.id },
+            body,
+            { new: true, runValidators: true })
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'Thought not found.'});
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(e => res.status(400).json(e))        
+    },
+
+    // delete thought
+    deleteThought({ params }, res) {
+        Thought.findOneAndDelete({ _id: params.id })
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'Thought not found.'})
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(e => res.status(404).json(e));
     }
 }
 
